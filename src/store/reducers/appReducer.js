c92eb58967e9349a8a8800c6a1dc573eae44e32e
Terence Mangram <tmangram@gmail.com>
2018-04-23 16:25:29 -0500

    Add List Item action logic, cleaned up code

--- src/store/reducers/appReducer.js
+++ src/store/reducers/appReducer.js
@@ -92,26 +92,68 @@ const reducer = (state = initialState, action) => {
 		case "DELETE_LIST_FAIL":
 			return {
 				...state,
+				error: action.error
 			};
 		case "ADD_LIST_ITEM_SUCCESS":
+			const newListItem = {
+				...action.payload,
+				id: action.listItemId
+			};
+			const newBoardsforListItem = state.boards.map(board => {
+				if(board.id !== action.boardId) {
+					return board;
+				}
+				const targetBoardLists = board.lists.map(list => {
+					if(list.id !== action.listId) {
+						return list;
+					}
+					const targetList = {
+						...list,
+						text: list.text.concat(newListItem)
+					};
+					return targetList;
+				})
+				const targetBoardforListItem = {
+					...board,
+					lists: targetBoardLists
+				};
+				return targetBoardforListItem;
+			});
 			return {
 				...state,
+				boards: newBoardsforListItem,
+				loading: false,
+				error: null
 			};
 		case "ADD_LIST_ITEM_FAIL":
 			return {
 				...state,
+				error: action.error
 			};
 		case "DELETE_LIST_ITEM_SUCCESS":
+		const newBoardsForItemDelete = state.boards.map(board => {
+			const newBoardList = board.lists.map(list => {
+				return {
+					...list,
+					text: list.text.filter(text => text.id !== action.listItemId)
+				};
+			});
+			const newBoardInItemDelete = {
+				...board,
+				lists: newBoardList 
+			};
+			return newBoardInItemDelete;
+		});
 			return {
 				...state,
+				boards: newBoardsForItemDelete,
+				loading: false,
+				error: null
 			};
 		case "DELETE_LIST_ITEM_FAIL":
 			return {
 				...state,
-			};
-		case "TOGGLE_LIST_ITEM":
-			return {
-				...state,
+				error: action.error
 			};
 		case "FETCH_BOARDS_SUCCESS":
 			return {
