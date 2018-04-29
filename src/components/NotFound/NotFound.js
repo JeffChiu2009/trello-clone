4a83edefba9b376ff6fcc8aa34dd3d3750ab35e6
Terence Mangram <tmangram@gmail.com>
2018-04-28 19:43:41 -0500

    Add top container, card-panel styling, class for redirect link

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-  <div>
-    <h1>Woops!</h1>
-    <Link to="/">Head back to safety!</Link>
+  <div className="boards-container">
+  	<div className="card-panel center">
+	    <h1>Woops!</h1>
+	    <Link className="redirect" to="/">Head back to safety!</Link>
+  	</div>
   </div>
 )
 
