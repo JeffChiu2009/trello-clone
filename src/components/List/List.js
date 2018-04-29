f85862efaf37edcb4d9b609ccdd7d24e6bf55b76
Terence Mangram <tmangram@gmail.com>
2018-04-29 06:02:33 -0500

    Add conditional styling for items, adjust close icons, li now handles click event

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,35 +1,35 @@
 import React from "react";
 
 const list = props => {
+	const checked = "list-item checked";
+	const unchecked = "list-item";
 	let listItems = null;
 	if(props.list.items) {
 		listItems = props.list.items.map(item =>
-			<li key={item.id}>
-				<p 
-				className={item.checked ? "checked" : ""}
-				onClick={() => props.onItemClick(item.id, props.list.id, props.boardId, item.checked, props.token)}>
-					{item.text}
-				</p>
+			<li 
+			key={item.id} 
+			className={item.checked ? checked : unchecked}
+			onClick={() => props.onItemClick(item.id, props.list.id, props.boardId, item.checked, props.token)}>
 				<div
-				onClick={() => props.onDeleteListItem(item.id, props.list.id, props.token, props.boardId)}
-				className="right close-icon">
-					<i className="material-icons">close</i>
+				onClick={() => props.onDeleteListItem(item.id, props.list.id, props.token, props.boardId)}>
+					<i className="material-icons right">close</i>
 				</div>
+				<p>{item.text}</p>
 			</li>
 		);
 	}
 	return (
-		<div className="card-panel list">
+		<div className="list">
 			<div
-			onClick={() => props.onDeleteList(props.list.id, props.token, props.boardId)}
-			className="right close-icon">
-				<i className="material-icons">close</i>
+			onClick={() => props.onDeleteList(props.list.id, props.token, props.boardId)}>
+				<i className="material-icons right">close</i>
 			</div>
-			{props.list.title}
+			<h3 className="catchy-title center">{props.list.title}</h3>
 			<ul>
 				{listItems}
 				<li>
-					<form onSubmit={(event) => props.handleAddListItem(event, props.list.id)}>
+					<form 
+					onSubmit={(event) => props.handleAddListItem(event, props.list.id)}>
 						<input type="text" name="text" />
 					</form>
 				</li>
