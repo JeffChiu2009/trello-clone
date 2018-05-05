0064f7d2f08ac62f7fa0aa1dab9a7e616b6169b5
Terence Mangram <tmangram@gmail.com>
2018-05-04 23:37:17 -0500

    Displays info about me

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const footer = props => (
+	<div id="footer">
+		<div className="container">
+			<p>Created by <a href="www.terencemangram.com" className="mine">Terence Mangram</a> using React, React-Router-Dom, Redux, Redux-Thunk, and Firebase.</p>
+		</div>
+	</div>
+);
+
+export default footer;
