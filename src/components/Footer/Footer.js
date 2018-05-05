d11a9d311418a93fea1452272a3923a98f805b2a
Terence Mangram <tmangram@gmail.com>
2018-05-05 18:28:19 -0500

    Correct href url

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const footer = props => (
 	<div id="footer">
 		<div className="container">
-			<p>Created by <a href="www.terencemangram.com" className="mine">Terence Mangram</a> using React, React-Router-Dom, Redux, Redux-Thunk, and Firebase.</p>
+			<p>Created by <a href="http://www.terencemangram.com" className="mine">Terence Mangram</a> using React, React-Router-Dom, Redux, Redux-Thunk, and Firebase.</p>
 		</div>
 	</div>
 );
