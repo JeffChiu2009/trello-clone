c4999198aaa9f0bfc5b6f4229d4242558bb8a306
Terence Mangram <tmangram@gmail.com>
2018-05-04 23:35:45 -0500

    Add redirect to NotFound's link

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NotFound = () => (
+const NotFound = props => (
   <div className="boards-container">
   	<div className="card-panel center">
 	    <h1>Woops!</h1>
-	    <Link className="redirect" to="/">Head back to safety!</Link>
+	    <Link className="redirect" to={props.isAuth ? "/boards" : "/"}>
+		    Head back to safety!
+	    </Link>
   	</div>
   </div>
 )
